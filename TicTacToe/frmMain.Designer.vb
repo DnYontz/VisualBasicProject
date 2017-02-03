3c3569d9a0c40d230b36586309db3c082905dca7
DYontz8981 <DYontz8981@WS-AC-CTD-37213>
2017-02-02 20:29:00 -0500

    Add project files.

---
+++ TicTacToe/frmMain.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.lblDirections = New System.Windows.Forms.Label()
+        Me.btnBegin = New System.Windows.Forms.Button()
+        Me.btnDone = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnOne
+        '
+        Me.btnOne.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOne.Location = New System.Drawing.Point(47, 79)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(76, 54)
+        Me.btnOne.TabIndex = 0
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnTwo.Location = New System.Drawing.Point(149, 79)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(76, 54)
+        Me.btnTwo.TabIndex = 1
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnThree.Location = New System.Drawing.Point(259, 79)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(76, 54)
+        Me.btnThree.TabIndex = 2
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnFour.Location = New System.Drawing.Point(47, 156)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(76, 54)
+        Me.btnFour.TabIndex = 3
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnFive.Location = New System.Drawing.Point(149, 156)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(76, 54)
+        Me.btnFive.TabIndex = 4
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnSix.Location = New System.Drawing.Point(259, 156)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(76, 54)
+        Me.btnSix.TabIndex = 5
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnSeven
+        '
+        Me.btnSeven.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnSeven.Location = New System.Drawing.Point(47, 235)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(76, 54)
+        Me.btnSeven.TabIndex = 6
+        Me.btnSeven.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnEight.Location = New System.Drawing.Point(149, 235)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(76, 54)
+        Me.btnEight.TabIndex = 7
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold)
+        Me.btnNine.Location = New System.Drawing.Point(259, 235)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(76, 54)
+        Me.btnNine.TabIndex = 8
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'lblDirections
+        '
+        Me.lblDirections.AutoSize = True
+        Me.lblDirections.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDirections.Location = New System.Drawing.Point(145, 30)
+        Me.lblDirections.Name = "lblDirections"
+        Me.lblDirections.Size = New System.Drawing.Size(78, 24)
+        Me.lblDirections.TabIndex = 9
+        Me.lblDirections.Text = "BEGIN!"
+        '
+        'btnBegin
+        '
+        Me.btnBegin.Location = New System.Drawing.Point(104, 329)
+        Me.btnBegin.Name = "btnBegin"
+        Me.btnBegin.Size = New System.Drawing.Size(75, 23)
+        Me.btnBegin.TabIndex = 10
+        Me.btnBegin.Text = "BEGIN"
+        Me.btnBegin.UseVisualStyleBackColor = True
+        '
+        'btnDone
+        '
+        Me.btnDone.Location = New System.Drawing.Point(201, 329)
+        Me.btnDone.Name = "btnDone"
+        Me.btnDone.Size = New System.Drawing.Size(75, 23)
+        Me.btnDone.TabIndex = 11
+        Me.btnDone.Text = "DONE"
+        Me.btnDone.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(391, 381)
+        Me.Controls.Add(Me.btnDone)
+        Me.Controls.Add(Me.btnBegin)
+        Me.Controls.Add(Me.lblDirections)
+        Me.Controls.Add(Me.btnNine)
+        Me.Controls.Add(Me.btnEight)
+        Me.Controls.Add(Me.btnSeven)
+        Me.Controls.Add(Me.btnSix)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Name = "frmMain"
+        Me.Text = "Tic-Tac-Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents lblDirections As Label
+    Friend WithEvents btnBegin As Button
+    Friend WithEvents btnDone As Button
+End Class
